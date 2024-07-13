d159e544ab73e96617cd582cf05a2baa01740929
lakkula <lakkula@gmail.com>
2024-07-13 18:47:28 +0530

    create NotificationSection

---
+++ src/components/notificationSection/MailIcon.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MailIcon() {
+  return (
+    <img src='' alt='mail'/>
+  )
+}
--- src/layouts/common/navigationWrapper/topNavigationLayout/NotificationSection.js
+++ src/layouts/common/navigationWrapper/topNavigationLayout/NotificationSection.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import MailIcon from '../../../../components/notificationSection/MailIcon'
+import NotificationIcon from '../../../../components/notificationSection/NotificationIcon'
 
 export default function NotificationSection() {
   return (
-    <div>NotificationSection</div>
+    <div style={{display:'flex', gap:'30px'}}>
+      <MailIcon/>
+      <NotificationIcon/>
+    </div>
   )
 }
